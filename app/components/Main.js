dcc3b0cfd231af598aca0e97a5d27f840d9608b9
dezoxel <dezoxel@gmail.com>
2015-07-02 14:59:51 +0300

    [Env] Up & Run, first component

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    )
+  }
+});
+
+React.render(<Main />, document.getElementById('app'));
+
+module.exports = Main;
